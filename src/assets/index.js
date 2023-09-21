6a6e25cdf7e5648b1b51474f107dcea5ae73ab38
Alvarosanti <alvarosantisteban56@gmail.com>
2023-09-21 13:46:37 -0500

    delete unnecessary files

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,27 +6,12 @@ import web from "./web.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
 
-import css from "./tech/css.png";
-import docker from "./tech/docker.png";
-import figma from "./tech/figma.png";
-import git from "./tech/git.png";
-import html from "./tech/html.png";
-import javascript from "./tech/javascript.png";
-import mongodb from "./tech/mongodb.png";
-import nodejs from "./tech/nodejs.png";
-import reactjs from "./tech/reactjs.png";
-import redux from "./tech/redux.png";
-import tailwind from "./tech/tailwind.png";
-import typescript from "./tech/typescript.png";
-import threejs from "./tech/threejs.svg";
-
 import info from "./company/info.png";
 import medicion from "./company/medicion.png";
 import propuesta from "./company/propuesta.png";
 import gestion from "./company/gestion.png";
 import delogo from "../assets/delogo.png"
 
-import tripguide from "./tripguide.png";
 
 export {
   logo,
@@ -36,23 +21,9 @@ export {
   web,
   menu,
   close,
-  css,
-  docker,
-  figma,
-  git,
-  html,
-  javascript,
-  mongodb,
-  nodejs,
-  reactjs,
-  redux,
-  tailwind,
-  typescript,
-  threejs,
   info,
   medicion,
   propuesta,
   gestion,
-  tripguide,
   delogo
 };
--- src/constans/index.js
+++ src/constans/index.js
@@ -3,23 +3,10 @@ import {
   backend,
   creator,
   web,
-  javascript,
-  typescript,
-  html,
-  css,
-  reactjs,
-  redux,
-  tailwind,
-  nodejs,
-  mongodb,
-  git,
-  figma,
-  docker,
   info,
   medicion,
   propuesta,
   gestion,
-  threejs,
 } from "../assets";
 
 export const navLinks = [
@@ -53,61 +40,6 @@ const services = [
   {
     title: "...",
     icon: creator,
-  },
-];
-
-const technologies = [
-  {
-    name: "HTML 5",
-    icon: html,
-  },
-  {
-    name: "CSS 3",
-    icon: css,
-  },
-  {
-    name: "JavaScript",
-    icon: javascript,
-  },
-  {
-    name: "TypeScript",
-    icon: typescript,
-  },
-  {
-    name: "React JS",
-    icon: reactjs,
-  },
-  {
-    name: "Redux Toolkit",
-    icon: redux,
-  },
-  {
-    name: "Tailwind CSS",
-    icon: tailwind,
-  },
-  {
-    name: "Node JS",
-    icon: nodejs,
-  },
-  {
-    name: "MongoDB",
-    icon: mongodb,
-  },
-  {
-    name: "Three JS",
-    icon: threejs,
-  },
-  {
-    name: "git",
-    icon: git,
-  },
-  {
-    name: "figma",
-    icon: figma,
-  },
-  {
-    name: "docker",
-    icon: docker,
   },
 ];
 
@@ -193,4 +125,4 @@ const testimonials = [
   },
 ];
 
-export { services, technologies, experiences, testimonials };
+export { services, experiences, testimonials };
